e9cff30cc4160fdae4e56d3d4516f0ac00fad860
Ronald Hualpa <20162130@aloe.ulima.edu.pe>
2020-01-29 11:22:05 -0500

    Login data repository completo

--- ms1-login/src/main/java/com/app/lotery/mslogin/repository/DataRepository.java
+++ ms1-login/src/main/java/com/app/lotery/mslogin/repository/DataRepository.java
@@ -1,5 +1,11 @@
 package com.app.lotery.mslogin.repository;
 
-public class DataRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.app.lotery.mslogin.model.LoginData;
+
+@Repository
+public interface DataRepository extends JpaRepository <LoginData, Long>{
 
 }
