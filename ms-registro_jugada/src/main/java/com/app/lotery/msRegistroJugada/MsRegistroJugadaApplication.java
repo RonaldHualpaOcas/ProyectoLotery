4187d6eb4333cbf452ee70f093708732419ac907
Ronald Hualpa <20162130@aloe.ulima.edu.pe>
2020-01-30 09:40:49 -0500

    Agregando microservicio Registro Jugada

---
+++ ms-registro_jugada/src/main/java/com/app/lotery/msRegistroJugada/MsRegistroJugadaApplication.java
@@ -0,0 +1,13 @@
+package com.app.lotery.msRegistroJugada;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsRegistroJugadaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsRegistroJugadaApplication.class, args);
+	}
+
+}
