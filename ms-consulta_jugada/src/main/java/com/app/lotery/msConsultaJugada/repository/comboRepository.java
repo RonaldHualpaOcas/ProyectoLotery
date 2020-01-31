cad4d6dd2760f3d9efc3b55f053a7395a23c960e
Ronald Hualpa <20162130@aloe.ulima.edu.pe>
2020-01-31 10:26:55 -0500

    Checkpoint

--- ms-consulta_jugada/src/main/java/com/app/lotery/msConsultaJugada/repository/comboRepository.java
+++ ms-consulta_jugada/src/main/java/com/app/lotery/msConsultaJugada/repository/comboRepository.java
@@ -1,5 +1,10 @@
 package com.app.lotery.msConsultaJugada.repository;
 
-public interface comboRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import com.app.lotery.msConsultaJugada.model.Combo_Entity;
+
+@Repository
+public interface comboRepository extends JpaRepository<Combo_Entity, Long>{
 
 }
