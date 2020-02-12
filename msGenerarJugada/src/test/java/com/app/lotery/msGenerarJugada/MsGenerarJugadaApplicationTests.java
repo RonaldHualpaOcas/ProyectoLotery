5c9df3139636a04919b132ab68b33cc05d80245d
Ronald Hualpa <20162130@aloe.ulima.edu.pe>
2020-02-12 14:54:43 -0500

    Archivos iniciales para Generar Jugada

---
+++ msGenerarJugada/src/test/java/com/app/lotery/msGenerarJugada/MsGenerarJugadaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.lotery.msGenerarJugada;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsGenerarJugadaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
